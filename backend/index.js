436c45f0eef7036ba569156312fbb0ec0966586e
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2024-10-11 23:55:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,3 @@
-// p - 3lBAaYlLnrMGqXNw
-// id - keven3605y@gmail.com
-// mongodb+srv://keven3605y:3lBAaYlLnrMGqXNw@cluster0.ld2av.mongodb.net/?retryWrites=true& w=majority&appName=Cluster0
-
 import dotenv from "dotenv";
 import express from 'express';
 import cors from 'cors';
@@ -20,11 +16,6 @@ app.use(cors({ origin : "http://localhost:5173" , credentials : true}));
 app.use(express.json()); // allows us to parse incoming requests : req.body
 app.use(cookieParser()); // allows us to parse incoming cookies 
 
-/*
-app.get("/" , (req, res) => {
-    res.send("Hello world ");
-});
-*/
 
 app.use("/api/auth" , authRoutes);
 
