d063c24b86ef85d67161b4a9d36044938ac797c6
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2024-12-14 20:00:23 +0530

    gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -233,7 +233,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 
 import SignUpPage from "./Pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./Pages/LoginPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
 import DashboardPage from "./pages/DashboardPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
@@ -245,7 +245,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore";
 import { useEffect } from "react";
 
-// protect routes that require authentication
+
 const ProtectedRoute = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
 
@@ -260,7 +260,7 @@ const ProtectedRoute = ({ children }) => {
 	return children;
 };
 
-// redirect authenticated users to the home page
+
 const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
 
@@ -332,7 +332,7 @@ function App() {
 						</RedirectAuthenticatedUser>
 					}
 				/>
-				{/* catch all routes */}
+			
 				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
 			<Toaster />
