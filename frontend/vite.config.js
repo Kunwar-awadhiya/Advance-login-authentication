b29ed5f8df1117beca4511465e58a142777bc245
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2024-12-14 20:46:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 
-
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,3 +7,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+*/
+
+
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+};
